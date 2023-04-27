8066f8176ce6d3beb7f3543ddf54a946942798f0
TaitKaito <clayton.h.c123@gmail.com>
2023-04-27 02:31:24 +0100

    Initial push for Dev Server point mod

---
+++ Data/Scripts/Additions/PointAdditions.cs
@@ -0,0 +1,43 @@
+﻿using Sandbox.ModAPI;
+using VRage.Game;
+using VRage.Game.Components;
+
+namespace ShipPoints
+{
+	[MySessionComponentDescriptor(MyUpdateOrder.NoUpdate)]
+    class PointAdditions : MySessionComponentBase
+    {
+        public override void Init(MyObjectBuilder_SessionComponent sessionComponent)
+        {
+            MyAPIGateway.Utilities.SendModMessage(2546247, MyAPIGateway.Utilities.SerializeToBinary(@"
+				
+				6InchTriple@200;
+				381mmDualR@200;
+				381mmDualNR@200;
+				380mmMLE1935@400;
+				15cmSKC28R@100;
+				15cmSKC28NR@100;
+				128mmL45@100;
+				128mmSKC34@115;
+				127mmMk12@200;
+				127mmMk24@225;
+				127mmMk32@250;
+				127mmMk56@350;
+				203mm50MLE1931_NR@225;
+				203mm50MLE1931_R@225;
+				105mmTwin@100;
+				152mmMLE1930NR@145;
+				152mmMLE1930R@145;
+				152mmMLE1936HA@200;
+				PomPomMain@100;
+				150mmCasemate@150;
+				BoforTwinRemodel@95;
+				BoforSingleRemodel@75;
+				QuadBofor@150;
+				TorpTestBuidl@350;
+				16InchTriple@400;
+
+            "));
+        }
+    }
+}
